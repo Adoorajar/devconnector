d1546b2e0320c4be82a6e00eaa865ab3009cf122
Dobbs <Brian.Dobbs@gdit.com>
2021-10-21 07:18:27 -0500

    Switch the app to using dotenv for environment vars config

--- config/db.js
+++ config/db.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURI')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        })
+  console.log(`In DB, MongoURI is: ${process.env.MONGO_URI}`);
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    });
 
-        console.log('MongoDB connected...')
-    } catch(err) {
-        console.error(err.message)
-        // exit process with failure
-        process.exit(1)
-    }
-}
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,76 +1,79 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const config = require('config')
-const { check, validationResult } = require('express-validator/check')
-const auth = require('../../middleware/auth')
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
-const User = require('../../models/User')
+const User = require('../../models/User');
 
 // @route    GET api/auth
 // @desc     Get user by token
 // @access   Private
 router.get('/', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
-// @route   POST api/auth 
+// @route   POST api/auth
 // @desc    Authenticate user & get token
-// @access  Public 
-router.post('/', [
+// @access  Public
+router.post(
+  '/',
+  [
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').exists()
-],
-async (req, res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    } 
-
-    const { email, password } = req.body
-
-    try {
-        let user = await User.findOne({ email })
-
-        if(!user) {
-            return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password)
-
-        if(!isMatch) {
-            return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
-        }
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign(
-            payload, 
-            config.get('jwtSecret'),
-            { expiresIn: 360000 },
-            (err, token) => {
-                if(err) throw err
-                res.json({ token })
-            }
-        )
-
-    } catch(err) {
-        console.error(err.message)
-        res.status(500).send('Server error')
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    
-})
+    const { email, password } = req.body;
 
-module.exports = router
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
